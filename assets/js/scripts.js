984966c753ba4db314cdef617771c10a20b69542
rakib <rakibhosen2244@gmail.com>
2024-02-15 11:22:42 +0600

    This is first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	// Mobile Menu
+	// $('.logo a i').click(function(){
+	// 	$('.menu ul').slideToggle(1000);
+
+	// 	return false
+	// });
+
+	// Banner Carousel
+	// $('.banner_carousel').owlCarousel({
+	// 	items:1,
+	// 	loop:true,
+	// 	nav:true,
+	// 	dots:true,
+	// 	autoplay:true,
+	// });
+
+
+
+
+
+});
